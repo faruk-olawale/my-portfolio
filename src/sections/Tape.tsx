b6419b22c2c4abe232ef5be23cb58996391d4048
faruk <farukolawale509@gmail.com>
2025-12-03 09:20:15 +0100

    initial message

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -6,6 +6,7 @@ const words = [
   "Interactive",
   "Scalable",
   "User Friendly",
+  "Responsive",
   "Maintainable",
   "Search Optimized",
   "Usable",
@@ -13,14 +14,14 @@ const words = [
 ]
 export const TapeSection = () => {
   return( 
-   <div className="py-16">
-    <div className="bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip">
-      <div className="flex [mask-image:lineear-gradient(to_right,transparent,black_90,transparent)]">
+   <div className="py-16 lg:py-24 overflow-x-clip">
+    <div className="bg-gradient-to-r from-emerald-300 to-sky-400  -rotate-3 -mx-1">
+      <div className="flex [mask-image: linear-gradient(to right, transparent, black 90%, transparent]">
     <div className="flex flex-none gap-4 py-3">
    {words.map(word => (
     <div key={word} className="inline-flex gap-4 items-center">
       <span className="text-gray-900 uppercase font-extrabold text-sm">{word}</span>
-      <StarIcon className="size-6 text-gray-900"/>
+      <StarIcon className="size-6 text-gray-900 -rotate-12"/>
     </div>
    ))}
   </div>
