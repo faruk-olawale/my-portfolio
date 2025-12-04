e54d1acf3256707e0cad4e9a93311daf74f55e94
faruk <farukolawale509@gmail.com>
2025-12-04 22:54:41 +0100

    initial message

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -57,7 +57,7 @@ const hobbies = [
     top:'5%',
   },
 
-  {
+    {
     title:"Hiking",
     emoji:"🥾",
     left:'35%',
@@ -69,6 +69,7 @@ const hobbies = [
     left:'10%',
     top:'35%',
   },
+
 
 
     {
@@ -94,7 +95,7 @@ const hobbies = [
 ]
 export const AboutSection = () => {
   return (
-  <div className="py-20">
+  <div className="py-20 lg:py-28">
   <div className="container max-w-5xl mx-auto px-4">
   <SectionHeader
    eyebrow="About me" 
@@ -102,26 +103,31 @@ export const AboutSection = () => {
    description="Learn more about who I am, What I do, and what Inspires me."
    />
    <div className="mt-20 flex flex-col gap-8">
-   <Card className="h-[320px]">
+    <div className="grid grid-cols-1 gap-8
+     md:grid-cols-5 ">
+   <Card className="h-[320px] md:col-span-2">
     <CardHeader title="My Reads" description="Explore the books shaping my perspectives."/>
-    <div className="w-40 mx-auto mt-8">
+    <div className="w-40 mx-auto mt-2 md:mt-0">
     <Image src={bookImage} alt="Book cover"/>
     </div>
    </Card>
    
-   <Card className="h-[320px] p-0">
+   <Card className="h-[320px]  md:col-span-3 ">
       <CardHeader 
       title="My Toolbox" 
       description="Explore the technologies and tools I use to craft exceptional digital experiences"
-      className="px-6 pt-6"
+      className=""
       />
      <ToolboxItems toolboxItems={toolboxItems} className="mt-6"/>
      <ToolboxItems 
      toolboxItems={toolboxItems} 
-     className="mt-6 "
+     className=" "
       itemsWrapperClassName="-translate-x-1/2" />
    </Card>
-   <Card className="h-[320px] p-0 flex flex-col">
+    </div>
+    <div className="grid grid-cols-1
+     md:grid-cols-5 gap-8">
+   <Card className="h-[320px] p-0 flex flex-col col-span-3">
       <CardHeader 
       title="Beyond the Code" 
       description="Explore my interests and hobbies beyond the digital realm"
@@ -146,10 +152,23 @@ export const AboutSection = () => {
       ))}
     </div>
    </Card>
-   <Card>
-    <Image src={mapImage} alt="map"/>
-    <Image src={smileEmoji} alt="smiling memoji"/>
+   <Card className="h-[320px] p-0 relative col-span-2">
+    <Image src={mapImage} alt="map"
+     className="h-full w-full object-cover
+     object-left-top"/>
+     <div className="absolute top-1/2 left-1/2
+     -translate-x-1/2 -translate-y-1/2 size-20
+      rounded-full bg-gradient-to-r
+     from-emerald-300 to-sky-400
+      after:content-[''] after:absolute 
+      after:inset-0 after:outline
+      after:outline-2 after:-outline-offset-2
+      after:rounded-full after:outline-gray-950/30">
+    <Image src={smileEmoji} alt="smiling 
+    memoji" className="size-20"/>
+    </div>
    </Card>
+   </div>
    </div>
    </div>
   </div>
