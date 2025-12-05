9fdf367ecac4b41f7db60b8a0d6fc2125b37c23e
faruk <farukolawale509@gmail.com>
2025-12-05 20:02:08 +0100

    initial message

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,20 +1,44 @@
 import { PropsWithChildren } from "react"
+import { twMerge } from "tailwind-merge";
 
 export const HeroOrbit = ({
      children,
      size,
-     rotation
-    }: PropsWithChildren<{ size: number; rotation: number; }>) => {
+     rotation,
+     shouldOrbit = false,
+     shouldspin,
+     spinDuration,
+     orbitDuration,
+    }: PropsWithChildren<{ 
+       size: number;
+       rotation: number;
+       shouldOrbit?:boolean;
+       shouldspin?:boolean;
+       spinDuration?:string;
+       orbitDuration?:string; }>) => {
     return(
-       <div className=" absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
-       <div className="" style={{
+       <div className=" absolute left-1/2 top-1/2 -translate-x-1/2
+        -translate-y-1/2 z-20">
+         <div className={twMerge( !!shouldOrbit === true &&" animate-spin ")} style={{
+            animationDuration: orbitDuration,
+         }}>
+       <div className="
+       " style={{
         transform:`rotate(${rotation}deg)`,
         height:`${size}px`,
         width:`${size}px`
        }}>
-       <div className=" inline-flex" style={{
+         <div className={twMerge(shouldspin === true && "animate-spin")} 
+         style={{
+            animationDuration: spinDuration
+         }}>
+       <div 
+        className=" inline-flex" style={{
         transform:`rotate(${rotation * -1}deg)`
-       }}>{children}</div>
+       }}>{children}
+       </div>
+       </div>
+       </div>
        </div>
        </div>
     )
