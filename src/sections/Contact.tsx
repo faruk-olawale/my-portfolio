105eb01d2faf97e7ea75d8486fd80025ca514b38
faruk <farukolawale509@gmail.com>
2025-12-08 09:15:47 +0100

    initial message

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -18,7 +18,7 @@ export const ContactSection = () => {
     </p>
     </div>
     <div>
-   <a href="mailto:farukolawale509@gmail.com">Contact Me
+   <a href="mailto:farukolawale509@gmail.com">
     <button className="text-white bg-gray-900 inline-flex 
       items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900">
       Contact Me
